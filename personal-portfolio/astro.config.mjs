ebc9cbd12c9f46f21673eed0b5f18111528e34b9
robsonl16 <117456362+robsonl16@users.noreply.github.com>
2025-09-25 18:46:04 -0400

    Hopefully fixing route issues

--- personal-portfolio/astro.config.mjs
+++ personal-portfolio/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://robsonl16.github.io",
-  base: "/PersonalPage/",
+  site: "https://robsonl16.github.io/PersonalPage/",
+  // base: "/PersonalPage/",
 });
